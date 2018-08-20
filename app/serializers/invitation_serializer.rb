0cf17aefc45922e3408ad1a72cec0b7d5bfe480d
Denis Zaratan Pasin <denis@pasin.fr>
2018-08-19 22:24:17 -0400

    Annotate serializer

--- app/serializers/invitation_serializer.rb
+++ app/serializers/invitation_serializer.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: invitations
+#
+#  id         :uuid             not null, primary key
+#  accepted   :boolean          default(FALSE)
+#  lesson_id  :uuid
+#  student_id :uuid
+#  teacher_id :uuid
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class InvitationSerializer < ActiveModel::Serializer
   attributes :id, :accepted, :student_id, :teacher_id
 end
